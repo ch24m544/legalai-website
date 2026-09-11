bbc4eaf814e861fef0998d17c035f0f8c056875e
ch24m544 <ch24m544@smail.iitm.ac.in>
2026-09-11 15:35:25 +0530

    Add robots configuration1

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://madhukaraadvocate.in/sitemap.xml',
+    sitemap: 'https://madhukaradvocate.in/sitemap.xml',
   }
 }
